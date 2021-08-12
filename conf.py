3e4940e5e1bf94afa5cbc60d65b8dd822f5ef2e8
XetPy1030 <xetpy1030@mail.ru>
2021-08-12 22:49:55 +0500

    mycommit

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 cell_char = "!"
 bot_word = ""
-commands = ["help", "list", "set", "enable", "profile", "delete"]
+commands = ["help", "list", "set", "profile", "enable", "delete"]
 bot_token = "ODQzODcyNzE4MTU4ODIzNDU1.YKKLtg.EceF6RqaCdYuTwpViMeVG42f3_U"
